81c343b87397bfc3182ad418dde442894e6bbe01
jasminet2001 <jasminet2001>
2024-12-28 18:10:28 +0330

    projects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
       "avatars.githubusercontent.com",
       "imgur.com",
       "media2.dev.to",
+      "jasminet2001.github.io",
+      "camo.githubusercontent.com",
+      "githubusercontent.com",
+      "private-user-images.githubusercontent.com",
     ],
   },
 };
