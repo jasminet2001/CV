1e740e7438873ebed169757270ddef8b262eca3d
jasminet2001 <jasminet2001>
2024-12-29 10:15:05 +0330

    finished projects section

--- next.config.js
+++ next.config.js
@@ -3,12 +3,15 @@ module.exports = {
     domains: [
       "res.cloudinary.com",
       "avatars.githubusercontent.com",
+      "user-images.githubusercontent.com",
       "imgur.com",
+      "github.com",
       "media2.dev.to",
       "jasminet2001.github.io",
       "camo.githubusercontent.com",
       "githubusercontent.com",
       "private-user-images.githubusercontent.com",
+      "inst.eecs.berkeley.edu",
     ],
   },
 };
